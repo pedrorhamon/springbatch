0087c0ec86f606d953cba030497a6ebd3fae2706
Pedro Rhamon <pedrorhamon16@gmail.com>
2022-10-29 20:03:58 -0300

    Implementação do primeiro projeto Spring Batch

---
+++ src/main/java/com/starking/springbatch/config/BatchConfig.java
@@ -0,0 +1,47 @@
+package com.starking.springbatch.config;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@EnableBatchProcessing
+@Configuration
+public class BatchConfig {
+	
+	@Autowired
+	private JobBuilderFactory jobBuilderFactory;
+	
+	@Autowired
+	private StepBuilderFactory stepBuilderFactory;
+	
+	@Bean
+	public Job imprimiJob() {
+		return jobBuilderFactory
+				.get("imprime")
+				.start(imprimeOla())
+				.build();		
+	}
+
+	public Step imprimeOla() {
+		return stepBuilderFactory
+				.get("imperime")
+				.tasklet(new Tasklet() {
+					
+					@Override
+					public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+						System.out.println("Ola mundo");
+						return RepeatStatus.FINISHED;
+					}
+				})
+				.build();
+	}
+}
